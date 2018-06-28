d2ea4cec3f2327567757858dc7b51062f6c4beea
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2018-06-28 09:18:50 -0500

    sqlite demo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import SQLiteDemo from './App';
 
-AppRegistry.registerComponent('lyrics', () => App);
+AppRegistry.registerComponent('lyrics', () => SQLiteDemo);
